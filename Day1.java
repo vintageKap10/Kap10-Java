1147bdf699bf711c095d2d2365fb01d56968272e
Vardhman Gupta <112063624+vintageKap10@users.noreply.github.com>
2022-12-27 17:48:33 +0000

    11:20 PM

--- Day1.java
+++ Day1.java
@@ -4,5 +4,50 @@
 public class Day1 {
     public static void main (String[] args) {
         System.out.println("Hello, Welcome back Chief!");
+
+        System.out.println("I am Currently Learning JAVA");
+
+        System.out.print("Hello ");
+        System.out.println("Bye");
+
+        /* DIfference b/w print & println is that print does not insert a new line at the end of the output */
+
+        System.out.println(6); // For number, no need to add ""
+        System.out.println(6+9);
+        System.out.println(6*9);
+
+        int myNUM = 15;
+        myNUM = 69; // changing value from 15 to 69 
+        System.out.println(myNUM);
+
+        // Other Type of Variables
+
+        int myNUm = 26;
+        float myFloatNum = 6.99f;
+        char myLetter = 'F';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(myNUm);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+        // Displaying varibales
+
+        String name = "Gupta"; //My Name
+        System.out.println("Vardhman " +name);
+
+        String firstName = "Ganeshi "; // College Name
+        String lastName = "Lal Bajaj";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        
+
+
+
+
     }
 }
