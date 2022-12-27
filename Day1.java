2aaf2c5fa2090dbe70289fe0f79a6f763d85e30f
Vardhman Gupta <112063624+vintageKap10@users.noreply.github.com>
2022-12-27 17:25:43 +0000

    Edit

---
+++ Day1.java
@@ -0,0 +1,8 @@
+// Use for Short Message
+/* Use for Long Message */
+
+public class Day1 {
+    public static void main (String[] args) {
+        System.out.println("Hello, Welcome back Chief!");
+    }
+}
